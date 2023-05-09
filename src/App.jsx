3380840a1ba6abccbafe390903700740aa5cb1cc
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-05-09 13:58:49 +0000

    add: react-awelsome-reveal

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import {
   Route,
 } from "react-router-dom";
 
+import { Slide } from "react-awesome-reveal";
+
 // Pages
 import { Home } from "./pages/Home/Home";
 import { Product } from "./pages/Product/Product";
@@ -13,11 +15,13 @@ import { CategoryProducts } from "./pages/CategoryProducts/CategoryProducts";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products/:id" element={<Product />} />
-        <Route path="/products/category/:category" element={<CategoryProducts />} />
-      </Routes>
+      <Slide>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:id" element={<Product />} />
+          <Route path="/products/category/:category" element={<CategoryProducts />} />
+        </Routes>
+      </Slide>
     </BrowserRouter>
   )
 }
